e3760d8863c1658981ecca31ec4952accba37112
Alex Nicolas <alexoliveira604@gmail.com>
2023-10-06 17:44:48 -0300

    feat: User model was created

---
+++ src/domain/model/user.go
@@ -0,0 +1,11 @@
+package model
+
+type User struct {
+	Id        int64  `json:"id"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Token     string `json:"token"`
+	CreatedAt int    `json:"created_at"`
+	UpdatedAt int    `json:"updated_at"`
+}
