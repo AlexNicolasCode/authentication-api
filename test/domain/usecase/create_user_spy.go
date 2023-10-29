dab680e787fff1befa29da153ee3f7d99fda8d3e
Alex Nicolas <alexoliveira604@gmail.com>
2023-10-29 13:41:53 -0300

    test: CreateUserRepository spy should be able to return error when test suite request

--- test/domain/usecase/create_user_spy.go
+++ test/domain/usecase/create_user_spy.go
@@ -1,16 +1,22 @@
 package mock
 
 import (
+	"errors"
+
 	protocol "finances-api/src/data/protocol/database"
 )
 
 type CreateUserRepository struct {
-	Count  int
-	Params protocol.CreateUserRepositoryParams
+	Count        int
+	Params       protocol.CreateUserRepositoryParams
+	ErrorMessage string
 }
 
 func (m *CreateUserRepository) CreateUser(params protocol.CreateUserRepositoryParams) error {
 	m.Count += 1
 	m.Params = params
+	if len(m.ErrorMessage) > 0 {
+		return errors.New(m.ErrorMessage)
+	}
 	return nil
 }
