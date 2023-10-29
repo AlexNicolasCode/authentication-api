0de90baa3b650edf3df393c068461ad9863a39ef
Alex Nicolas <alexoliveira604@gmail.com>
2023-10-28 21:29:59 -0300

    test: CreateUserRepository spy should be able to store params when called to enable test comparation

--- test/domain/usecase/create_user_spy.go
+++ test/domain/usecase/create_user_spy.go
@@ -5,10 +5,12 @@ import (
 )
 
 type CreateUserRepository struct {
-	Count int
+	Count  int
+	Params protocol.CreateUserRepositoryParams
 }
 
 func (m *CreateUserRepository) CreateUser(params protocol.CreateUserRepositoryParams) error {
 	m.Count += 1
+	m.Params = params
 	return nil
 }
