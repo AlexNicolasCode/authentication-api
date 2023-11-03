399af93b280c4692a21bd68c18e468606416854c
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-03 11:56:29 -0300

    test: CreateUser domain usecase should call hasher with correct param

--- test/data/usecase/create_user_test.go
+++ test/data/usecase/create_user_test.go
@@ -5,37 +5,47 @@ import (
 
 	"github.com/bxcodec/faker/v3"
 
+	cryptography "src/data/protocol/cryptography"
 	database "src/data/protocol/database"
 	domain "src/domain/usecase"
-	mock "test/domain/usecase"
+	mock_data "test/domain/usecase"
+	mock_infra "test/infra"
 )
 
 type UseCase struct {
 	create_user_repository database.CreateUserRepository
+	hasher                 cryptography.Hasher
 }
 
 func (uc *UseCase) CreateUser(params domain.CreateUserParams) error {
-	var repositoryParams database.CreateUserRepositoryParams
-	repositoryParams.Name = params.Name
-	repositoryParams.Email = params.Email
-	repositoryParams.Password = params.Password
-	err := uc.create_user_repository.CreateUser(repositoryParams)
+	hashedPassword, err := uc.hasher.Hash(params.Password)
+	if err != nil {
+		return err
+	}
+	repoParams := database.CreateUserRepositoryParams{
+		Name:     params.Name,
+		Email:    params.Email,
+		Password: hashedPassword,
+	}
+	err = uc.create_user_repository.CreateUser(repoParams)
 	return err
 }
 
-func MakeCreateUser(create_user_repository database.CreateUserRepository) UseCase {
-	return UseCase{create_user_repository}
+func MakeCreateUser(create_user_repository database.CreateUserRepository, hasher cryptography.Hasher) UseCase {
+	return UseCase{create_user_repository, hasher}
 }
 
 type SutSetupTypes struct {
 	sut               UseCase
-	createUserRepoSpy *mock.CreateUserRepository
+	createUserRepoSpy *mock_data.CreateUserRepository
+	hasher            *mock_infra.Hasher
 }
 
 func MakeSutSetup() SutSetupTypes {
-	createUserRepoSpy := new(mock.CreateUserRepository)
-	sut := MakeCreateUser(createUserRepoSpy)
-	return SutSetupTypes{sut, createUserRepoSpy}
+	createUserRepoSpy := new(mock_data.CreateUserRepository)
+	hasherSpy := new(mock_infra.Hasher)
+	sut := MakeCreateUser(createUserRepoSpy, hasherSpy)
+	return SutSetupTypes{sut, createUserRepoSpy, hasherSpy}
 }
 
 func MakeUserRequest() domain.CreateUserParams {
@@ -64,7 +74,7 @@ func TestShouldCallCreateUserRepositoryWithCorrectParams(t *testing.T) {
 
 	if setup.createUserRepoSpy.Params.Email != fakeParams.Email ||
 		setup.createUserRepoSpy.Params.Name != fakeParams.Name ||
-		setup.createUserRepoSpy.Params.Password != fakeParams.Password {
+		setup.createUserRepoSpy.Params.Password != setup.hasher.Result {
 		t.Error("CreateUser method from CreateUserRepository was called more than one time or not called")
 	}
 }
@@ -86,6 +96,17 @@ func TestShouldReturnNoneErrorOnSuccess(t *testing.T) {
 	err := setup.sut.CreateUser(MakeUserRequest())
 
 	if err != nil {
+		t.Error("CreateUser return some error")
+	}
+}
+
+func TestShouldCallHasherWithCorrectParam(t *testing.T) {
+	setup := MakeSutSetup()
+	fakeParams := MakeUserRequest()
+
+	setup.sut.CreateUser(fakeParams)
+
+	if setup.hasher.Plaintext != fakeParams.Password {
 		t.Error("CreateUser return some error")
 	}
 }
