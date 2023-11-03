2797ec7d2ab1fcdd4ceab087d9dd936a4c7869af
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-03 17:29:45 -0300

    feat: User model was created to use gorm and enable correct table creation

--- src/domain/model/user.go
+++ src/domain/model/user.go
@@ -1,11 +1,11 @@
 package domain
 
 type User struct {
-	Id        int64  `json:"id"`
-	Name      string `json:"name"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
-	Token     string `json:"token"`
-	CreatedAt int    `json:"created_at"`
-	UpdatedAt int    `json:"updated_at"`
+	Id        uint    `json:"id" gorm:"primaryKey"`
+	Name      string  `json:"name"`
+	Email     string  `json:"email" gorm:"not null"`
+	Password  string  `json:"password" gorm:"not null"`
+	Token     *string `json:"token"`
+	CreatedAt int64   `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt int64   `json:"updated_at" gorm:"autoUpdateTime:milli"`
 }
