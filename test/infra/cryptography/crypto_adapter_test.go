d34d181e63b78febf1181c01e899fefaf945547e
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-03 14:52:56 -0300

    test: CryptoAdapter should throw if Bcrypt throws

---
+++ test/infra/cryptography/crypto_adapter_test.go
@@ -0,0 +1,39 @@
+package cryptography
+
+import (
+	"math/rand"
+	"test/infra/mock"
+	"testing"
+
+	"github.com/bxcodec/faker/v3"
+)
+
+type CryptoAdapter struct {
+	salt                 int
+	GenerateFromPassword func(password []byte, cost int) ([]byte, error)
+}
+
+func (c *CryptoAdapter) Hash(plaintext string) (string, error) {
+	bytes, err := c.GenerateFromPassword([]byte(plaintext), c.salt)
+	return string(bytes), err
+}
+
+func NewCryptoAdapter(
+	GenerateFromPassword func(password []byte, cost int) ([]byte, error),
+	salt int,
+) CryptoAdapter {
+	return CryptoAdapter{salt, GenerateFromPassword}
+}
+
+func TestShouldThrowIfBcryptThrows(t *testing.T) {
+	randomInt := rand.Int()
+	bcryptSpy := new(mock.BcryptSpy)
+	bcryptSpy.ErrorMessage = "Mocked Error"
+	sut := NewCryptoAdapter(bcryptSpy.GenerateFromPassword, randomInt)
+
+	_, err := sut.Hash(faker.Password())
+
+	if err.Error() != bcryptSpy.ErrorMessage {
+		t.Error("CreateUser method from CreateUserRepository was called more than one time or not called")
+	}
+}
---
+++ test/infra/cryptography/go.mod
@@ -0,0 +1,3 @@
+module test/infra/cryptography
+
+go 1.18
